b79102f2be24693758a7bd7830efb58eff4649ae
Serhii Ovsiienko <sott095@gmail.com>
2021-05-29 13:21:10 +0300

    tesing request

---
+++ valueCount.py
@@ -0,0 +1,37 @@
+from gremlin_python.process.anonymous_traversal import traversal
+from gremlin_python.driver.driver_remote_connection import\
+    DriverRemoteConnection
+import json
+
+
+to_string = json.dumps
+
+
+def main():
+    remoteConn = DriverRemoteConnection('ws://172.17.0.2:8182/gremlin', 'g')
+    g = traversal().withRemote(remoteConn)
+
+    # Count
+    print(
+        "Count: " + to_string(
+            g
+            .V()
+            .count()
+            .next()
+        )
+    )
+    # Check values
+    print(
+        g.V().both().name.toList()
+    )
+
+    # Map and Return array dictionary nested
+    print(g.V().valueMap().toList())
+
+    # Group by
+    print(g.V().group().by('name').toList())
+    remoteConn.close()
+
+
+if __name__ == "__main__":
+    main()
